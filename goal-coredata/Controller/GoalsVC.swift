283d60b2a957ec4fe22f580e9f87fb92b0dcb45e
darklightsak <s.wattanapradit@gmail.com>
2019-01-23 01:18:41 +0700

    navbar setup and table view setup

--- goal-coredata/Controller/GoalsVC.swift
+++ goal-coredata/Controller/GoalsVC.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalsVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func addGoalPressed(_ sender: Any) {
+        print("pressed")
+    }
+    
 }
 
